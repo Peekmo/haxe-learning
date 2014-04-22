754deb285c20ae7e3d9d3b6d4f1cf71b5d0127aa
Peekmo <axel.anceau@gmail.com>
2014-04-22 20:54:23 +0200

    Tutorial part 2

--- Plateformer/source/PlayState.hx
+++ Plateformer/source/PlayState.hx
@@ -8,6 +8,8 @@ import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 import flixel.tile.FlxTilemap;
 import openfl.Assets;
+import flixel.group.FlxGroup;
+import flixel.FlxObject;
 
 /**
  * A FlxState which can be used for the actual gameplay.
@@ -17,6 +19,12 @@ class PlayState extends FlxState
 	private var map : FlxTilemap;
 
 	private var player : FlxSprite;
+
+	private var isJumping : Bool = false;
+
+	private var bananas : FlxGroup;
+
+	private var text : FlxText;
 
 	/**
 	 * Function that is called up when to state is created to set it up. 
@@ -30,11 +38,27 @@ class PlayState extends FlxState
 		this.player = new FlxSprite();
 		this.player.x = 40;
 		this.player.y = 70;
-		this.player.maxVelocity.set(80, 100);
-		this.player.loadGraphic("assets/images/player.png", true, true, 25, 34);
-		this.add(player);
 		this.player.acceleration.y = 100;
 		this.player.drag.x = 100;
+
+		this.player.loadGraphic("assets/images/player.png", true, true, 25, 34);
+		this.player.animation.add("default", [0,1], 3);
+		this.player.animation.add("jump", [2]);
+		this.player.animation.play("default");
+
+		this.add(player);
+
+		this.bananas = new FlxGroup();
+		this.add(this.bananas);
+		
+		for (i in 0...10) {
+			var banana : FlxSprite = new FlxSprite(Math.random() * this.map.width, Math.random() * this.map.height);
+			banana.loadGraphic("assets/images/collectible.png");
+			this.bananas.add(banana);
+		}
+
+		this.text = new FlxText(0, 0, 50, "- Welcome -");
+		this.add(this.text);
 
 		super.create();
 	}
@@ -54,18 +78,30 @@ class PlayState extends FlxState
 	override public function update():Void
 	{
 		super.update();
+		FlxG.collide(this.map, this.player);
+		FlxG.overlap(this.player, this.bananas, function(playerRef: FlxObject, collectibleRef: FlxObject) : Void {
+			this.bananas.remove(collectibleRef);
+		});
+
 		if (FlxG.keys.pressed.LEFT) {
 			this.player.velocity.x = -70;
+			this.player.facing = flixel.FlxObject.LEFT;
 		}
 
 		if (FlxG.keys.pressed.RIGHT) {
 			this.player.velocity.x = 70;
+			this.player.facing = flixel.FlxObject.RIGHT;
 		}
 
-		if (FlxG.keys.pressed.UP) {
+		if (0 == this.player.velocity.y && FlxG.keys.pressed.UP) {
 			this.player.velocity.y = -130;
+			this.player.animation.play("jump");
+			this.isJumping = true;
 		}
 
-		FlxG.collide(this.map, this.player);
+		if (this.isJumping && this.player.velocity.y == 0) {
+			this.isJumping = false;
+			this.player.animation.play("default");
+		}
 	}	
 }
